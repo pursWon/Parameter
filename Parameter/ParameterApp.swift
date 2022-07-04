3e544486a86003ef845032d322e867b64b66bf3f
Wongi <hpurs95@gmail.com>
2022-07-04 19:36:50 +0900

    Initial Commit

---
+++ Parameter/ParameterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParameterApp.swift
+//  Parameter
+//
+//  Created by MacBook Air on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct ParameterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
